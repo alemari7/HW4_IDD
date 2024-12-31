3aaaf053688eeb3722260f897c2fc2b07e15a496
Riccardo Felici <riccardofelici7@gmail.com>
2024-12-31 15:51:39 +0100

    Update task3.sh

--- task3.sh
+++ task3.sh
@@ -11,6 +11,17 @@ else
 fi
 
 # Esegui il secondo script Python
+# CODICE PER GENERARE IL DIZIONARIO DEI SINONIMI
+# NON ANCORA UTILIZZATO NEL MERGE
+python3 alignment/synonym_dict_generator.py
+if [ $? -eq 0 ]; then
+    echo "synonym_dict_generator completato con successo"
+else
+    echo "Errore nell'esecuzione di synonym_dict_generator"
+    exit 1
+fi
+
+# Esegui il terzo script Python
 python3 alignment/merge_alignment.py
 if [ $? -eq 0 ]; then
     echo "merge_profiling.py completato con successo"
